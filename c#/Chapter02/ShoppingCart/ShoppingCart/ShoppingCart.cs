623671f2969366d0872cc8c4851c2c052b839d68
Kiyotaka.Abe <kiyo7447@gmail.com>
2021-05-09 12:32:39 +0900

    update

---
+++ c#/Chapter02/ShoppingCart/ShoppingCart/ShoppingCart.cs
@@ -0,0 +1,41 @@
+namespace ShoppingCart.ShoppingCart
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using EventFeed;
+
+	public class ShoppingCart
+	{
+		private readonly HashSet<ShoppingCartItem> items = new();
+
+		public ShoppingCart(int userId) => this.UserId = userId;
+
+		public int UserId { get; }
+		public IEnumerable<ShoppingCartItem> Items => this.items;
+
+		public void AddItems(IEnumerable<ShoppingCartItem> shoppingCartItems, IEventStore eventStore)
+		{
+			foreach (var item in shoppingCartItems)
+				if (this.items.Add(item))
+					eventStore.Raise("ShoppingCartItemAdded", new { this.UserId, item });
+		}
+
+		public void RemoveItems(int[] productCatalogueIds, IEventStore eventStore) =>
+		  this.items.RemoveWhere(i => productCatalogueIds.Contains(i.ProductCatalogueId));
+	}
+
+	public record ShoppingCartItem(
+	  int ProductCatalogueId,
+	  string ProductName,
+	  string Description,
+	  Money Price)
+	{
+		public virtual bool Equals(ShoppingCartItem? obj) =>
+		  obj != null && this.ProductCatalogueId.Equals(obj.ProductCatalogueId);
+
+		public override int GetHashCode() => this.ProductCatalogueId.GetHashCode();
+	}
+
+	public record Money(string Currency, decimal Amount);
+}
